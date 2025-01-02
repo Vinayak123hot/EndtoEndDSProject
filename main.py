9a33291a8f0a01cea433dc61d24ec4fab8e62310
Vinayak123hot <bvssvinu@gmail.com>
2025-01-02 21:02:33 +0530

    almost done stage

--- main.py
+++ main.py
@@ -1,3 +1,45 @@
 from src.datascience import logger
+from src.datascience.pipeline.data_ingestion_pipelile import DataIngestionTrainingPipeline
+from src.datascience.pipeline.data_validation_pipeline import DataValidationTrainingPipeline
+from src.datascience.pipeline.data_transformation_pipeline import DataTransformationTrainingPipeline
 
-logger.info("welcome to our custom data science project log")
+STAGE_NAME= "Data Ingestion Stage"
+
+
+
+try:
+    logger.info(f">>>>>>>>>Stage {STAGE_NAME} started <<<<<<<<<<")
+    obj= DataIngestionTrainingPipeline()
+    obj.initiate_data_ingestion()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME}  completed<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME= "Data Validation Stage"
+try:
+    logger.info(f">>>>>>>>>Stage {STAGE_NAME} started <<<<<<<<<<")
+    obj= DataValidationTrainingPipeline()
+    obj.initiate_data_validation()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME}  completed<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME= "Data Transformation Stage"
+try:
+    logger.info(f">>>>>>>>>Stage {STAGE_NAME} started <<<<<<<<<<")
+    dt= DataTransformationTrainingPipeline()
+    dt.initiate_data_transformation()
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME}  completed<<<<<<<<")
+    
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+
+
---
+++ src/datascience/entity/config_entity.py
@@ -0,0 +1,22 @@
+from dataclasses import dataclass
+from pathlib import Path
+
+@dataclass
+class DataIngestionconfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
+    
+
+@dataclass
+class DataValidationconfig:
+    root_dir: Path
+    STATUS_FILE: str
+    unzip_data_dir: Path
+    all_schema: dict
+    
+@dataclass
+class DataTransformationConfig:
+    root_dir: Path
+    data_path: Path
